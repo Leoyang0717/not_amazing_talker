df315eb074bc8c241a57165da2986e9c2df24198
CKyang <pithend58639@gmail.com>
2021-11-13 10:24:03 +0800

    add tutor controller

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class Tutor < ApplicationRecord
   has_many :tutor_subjects
   has_many :subjects, through: :tutor_subjects
+
+  validates :name, presence: true
 end
